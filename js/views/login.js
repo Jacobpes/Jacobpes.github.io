de891596e396ed4d8de87f5d3c9e31ca086480e6
Jacobp <jacob.pesamaa@gritlab.ax>
2023-05-17 21:19:59 +0300

    err message

--- js/views/login.js
+++ js/views/login.js
@@ -38,8 +38,10 @@ class Login extends HTMLElement{
             }
         }
         catch (error) {
-           // set error message to "wrong username or password"
-              document.getElementById('loginErrorMessage').innerHTML = "Wrong username or password";
+            let loginErr = document.getElementById('loginUsernameErrMsg')
+            if (loginErr) {
+                loginErr.innerHTML = "Wrong username/email or password";;
+            }
         }
     }
     connectedCallback() {
@@ -109,40 +111,32 @@ function validLoginForm(formData){
 // check if username is empty
 if (formData.username == ""){
     valid.username = false;
- } else {
-     // check if usernameemail is valid username or email
-     const emailRegex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
-     const usernameRegex = new RegExp(/^[åäöÅÄÖA-Za-z0-9]+$/);
-     if (!(usernameRegex.test(formData.username) || emailRegex.test(formData.username))){
-         usernameErrMsg.innerHTML = "Username is invalid";
-         valid.username = false;
-     } else {
-         // else username is valid
-         usernameErrMsg.classList.remove("form__input-error-message");
-         usernameErrMsg.innerHTML = "";
-         valid.username = true;
-     }
- }
+    } else {
+        // check if usernameemail is valid username or email
+        const emailRegex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+        const usernameRegex = new RegExp(/^[åäöÅÄÖA-Za-z0-9]+$/);
+        if (!(usernameRegex.test(formData.username) || emailRegex.test(formData.username))){
+            usernameErrMsg.innerHTML = "Username is invalid";
+            valid.username = false;
+        } else {
+            // else username is valid
+            usernameErrMsg.classList.remove("form__input-error-message");
+            usernameErrMsg.innerHTML = "";
+            valid.username = true;
+        }
+    }
 
     // check if password is empty
     if (formData.password == ""){
         passwordErrMsg.classList.add("form__input-error-message");
         valid.password = false;
     } else {
-        // check if password is invalid allow åäöÅÄÖ 
-        const passwordLengthRegex = new RegExp(/^[åäöÅÄÖA-Za-z0-9]{5,}$/);
-        if (!passwordLengthRegex.test(formData.password)){
-            passwordErrMsg.innerHTML = "Password must be at least 5 characters";
-            valid.password = false;
-        } else {
-            // else password is valid
-            passwordErrMsg.classList.remove("form__input-error-message");
-            passwordErrMsg.innerHTML = "";
-            valid.password = true;
-        }
+        passwordErrMsg.classList.remove("form__input-error-message");
+        passwordErrMsg.innerHTML = "";
+        valid.password = true;
     }
     
-    // if all booleans are true, return true else return false
+    // if all booleans are true, return true, else return false
     if (valid.username == false || valid.password == false){
         return false;
     } else {
